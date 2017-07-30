6f504f0e6e7ad136120a565c1621e38c30aa09a0
John L. Singleton <jsinglet@gmail.com>
2017-07-30 18:05:56 -0400

    adding new asm file

---
+++ src/example/ASMTest4.java
@@ -0,0 +1,61 @@
+package example;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.evidently.agent.Flowpoint;
+import org.evidently.agent.FlowpointCollector;
+import org.evidently.agent.FlowpointInstrumenter;
+import org.evidently.agent.FlowpointVariableRewriter;
+import org.objectweb.asm.ClassWriter;
+
+public class ASMTest4 {
+
+	
+	public static void main(String[] args) throws IOException {
+
+		String clazz = "example/SomeClass";
+		
+		// first transformation
+		ClassWriter cw = null;
+		{
+			FlowpointCollector coll = new FlowpointCollector(clazz);
+			coll.collectFlowpoints();
+			
+			FlowpointVariableRewriter c = new FlowpointVariableRewriter(clazz, coll);
+			
+			cw = c.instrument();
+			
+			// first we scan over 
+			new File("bin-debug/example/").mkdirs();
+			FileOutputStream fos = new FileOutputStream("bin-debug/example/SomeClass-pass1.class");
+			fos.write(cw.toByteArray());
+			fos.close();
+		}
+
+		// second transformation (adds the update statements) 
+		{
+			FlowpointCollector coll = new FlowpointCollector(clazz, cw.toByteArray());
+			coll.collectFlowpoints();
+			
+			FlowpointInstrumenter c = new FlowpointInstrumenter(clazz, cw.toByteArray(), coll, new ArrayList<Class <? extends Flowpoint>>());
+			
+			cw = c.instrument();
+			
+			// first we scan over 
+			new File("bin-debug/example/").mkdirs();
+			FileOutputStream fos = new FileOutputStream("bin-debug/example/SomeClass-pass2.class");
+			fos.write(cw.toByteArray());
+			fos.close();
+
+		}
+		
+		
+		
+		
+
+	}
+
+}
