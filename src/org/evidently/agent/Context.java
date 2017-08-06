57d705e22ca5bd7972358fea02aa046cac638d5f
John L. Singleton <jsinglet@gmail.com>
2017-08-05 23:16:05 -0400

    new classes

---
+++ src/org/evidently/agent/Context.java
@@ -0,0 +1,5 @@
+package org.evidently.agent;
+
+public class Context {
+
+}
---
+++ src/org/evidently/agent/Property.java
@@ -0,0 +1,21 @@
+package org.evidently.agent;
+
+import java.util.List;
+
+public abstract class Property {
+
+	// name of this property
+	public abstract String getName(); 
+	
+	// other methods dynamically created. 
+	
+	// checks if the spec conforms
+	// public abstract boolean spec();
+	
+	//
+	// gets the value of the property
+	// 
+	// Pair<Taint,Object> getValue() // the computed taint and the object value. 
+	// 
+	
+}
